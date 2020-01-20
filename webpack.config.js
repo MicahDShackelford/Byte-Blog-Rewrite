a5c523ece3b618eba87eb32441c720f0d00a3feb
Micah <micahdshackelford@gmail.com>
2020-01-20 16:44:46 -0500

    Setup file structure, react, & express

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src',
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname,"dist"),
+        path: path.resolve(__dirname,"dist/resources/js")
     },
     module: {
         rules: [
@@ -15,7 +15,7 @@ module.exports = {
             use: {
               loader: 'babel-loader',
               options: {
-                presets: ['@babel/preset-env']
+                presets: ['@babel/preset-react','@babel/preset-env']
               }
             }
           }
